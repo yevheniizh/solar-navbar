6e2c2bcc85bbf4a3e79d103b6c9d962078d96e9d
Yevhenii Zh <yevheniizhyrov.dev@gmail.com>
2023-10-15 14:05:41 +0300

    Add solar navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import "./styles.css";
-import logo from "./assets/javascript-logo.png";
 
-const img = document.createElement("img");
-img.src = logo;
-document.getElementById("app").appendChild(img);
+const app = document.querySelector("#app");
+
+app.innerHTML = `
+  <div class='nav'>
+    <button class='nav-toggler'>
+      <span class='navbar-toggler-icon'></span>
+    </button>
+    <ul class='nav-list'>
+      <li class='nav-item'><span>One</span></li>
+      <li class='nav-item'><span>Two</span></li>
+      <li class='nav-item'><span>Three</span></li>
+      <li class='nav-item'><span>Four</span></li>
+      <li class='nav-item'><span>Five</span></li>
+      <li class='nav-item'><span>Six</span></li>
+      <li class='nav-item'><span>Seven</span></li>
+      <li class='nav-item'><span>Eight</span></li>
+    </ul>
+  </div>
+`;
+
+const nav = document.querySelector(".nav");
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  nav.classList.contains("opened")
+    ? nav.classList.remove("opened")
+    : nav.classList.add("opened");
+});
